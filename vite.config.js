be5e5afae5eedc6c33d0be56082a52b1c7044b6b
muntahics <muntahi05@outlook.com>
2025-05-28 00:56:38 +0100

    button animations

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/assembly-endgame"
+  // server: {
+  //   host: true, // or use '0.0.0.0'
+  //   port: 5713
+  // }
 })
